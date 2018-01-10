d23d8435c2058a623ac6b8d3b8ae535e89c08e79
asder707 <abn.sebick@gmail.com>
2018-01-10 09:29:29 -0300

    guardar y obtener imagenes

--- controllers/image.js
+++ controllers/image.js
@@ -7,6 +7,49 @@ function test(req,res){
   res.status(200).send({message: 'imagecontroller working'});
 };
 
+function getImage(req,res){
+  var imageId = req.params.id;
+  Image.findById(imageId, (err,image)=>{
+    if(err){
+      res.status(500).send({message: "ERROR in petition"});
+    }else{
+      if(!image){
+        res.status(404).send({message: "image not found!"});
+      }else{
+        Album.populate(image,{path: 'album'}, (err, image)=>{
+          if(err){
+            res.status(500).send({message: "ERROR in petition"});
+          }else{
+            res.status(200).send({image});
+          }
+        });
+      }
+    }
+  });
+}
+
+function saveImage(req, res){
+  var image = new Image();
+  var params = req.body;
+  image.title = params.title;
+  image.picture = null; //esto se hará internamente
+  image.album = params.album;
+
+  image.save((err,imageStored)=>{
+    if(err){
+      res.status(500).send({message: "ERROR in petition"});
+    }else{
+      if(!imageStored){
+        res.status(404).send({message: "Error in save!"});
+      }else{
+        res.status(200).send({image: imageStored});
+      }
+    }
+  });
+}
+
 module.exports = {
-  test
+  test,
+  getImage,
+  saveImage
 };
--- routes/image.js
+++ routes/image.js
@@ -4,6 +4,8 @@ var express = require('express');
 var ImageController = require('../controllers/image');
 var api = express.Router();
 
-api.get('/image', ImageController.test);
+api.get('/image-test', ImageController.test);
+api.get('/image/:id', ImageController.getImage);
+api.post('/image', ImageController.saveImage);
 
 module.exports = api;
